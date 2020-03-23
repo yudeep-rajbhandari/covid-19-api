0c18882a777caff624788923ae85b841c713d1c5
Rajbhandari, Yudeep <i28251@verisk.com>
2020-03-23 22:31:52 +0545

    added country wise search and current final data

--- src/main/java/com/example/RestController.java
+++ src/main/java/com/example/RestController.java
@@ -2,8 +2,10 @@ package com.example;
 
 import com.example.scrap.Scrapper;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,9 +16,9 @@ public class RestController {
     @Autowired
     private Scrapper scrapper;
 
-    @RequestMapping(value = "/allUpdate", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/allUpdate", method = RequestMethod.GET)
     @ResponseBody
-    public String createMessage() {
+    public String allcountryUpdate() {
 
         JSONArray array = scrapper.getdocument();
         if(array != null){
@@ -25,5 +27,37 @@ public class RestController {
         else{
             return new JSONArray().toString();
         }
+    }
+
+    @RequestMapping(value = "/update/country/{country}", method = RequestMethod.GET)
+    @ResponseBody
+    public String updatebyCountry(@PathVariable("country") final String country) {
+
+        JSONObject array = scrapper.getdocumentbyCountry(country);
+        if(array != null){
+            return  array.toString();
+        }
+        else{
+            return new JSONArray().toString();
+        }
+    }
+
+    @RequestMapping(value = "/update/currentTotalcases", method = RequestMethod.GET)
+    @ResponseBody
+    public String totalupdate() {
+
+        JSONObject array = scrapper.getTotalcases();
+        if(array != null){
+            return  array.toString();
+        }
+        else{
+            return new JSONArray().toString();
+        }
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome() {
+
+     return "Welcome to covid-19-data-centre";
     }
 }
