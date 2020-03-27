7a39c85b5e5f7107d1899e8130214c79a4eebc52
SergeyShustikov <pandarium.shustikov@gmail.com>
2020-03-27 17:49:40 +0200

    Cleanup

--- src/main/java/com/example/RestController.java
+++ src/main/java/com/example/RestController.java
@@ -4,11 +4,12 @@ import com.example.scrap.Scrapper;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@org.springframework.web.bind.annotation.RestController
+@Controller
 public class RestController {
 
     @Autowired
